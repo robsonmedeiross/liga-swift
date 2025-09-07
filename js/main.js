b269d96b58f711c0b9ed4e2488ade7a3d9d11a08
Robson Medeiros <idrosbonmedeiros@gmail.com>
2025-09-06 22:08:17 -0300

    Adiciona estrutura inicial do projeto Liga Swift com arquivos HTML, CSS e JavaScript, além de pastas para fontes, ícones e imagens.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Liga Swift - JavaScript Principal
+
+// Aguarda o carregamento completo do DOM
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Liga Swift - Aplicação carregada com sucesso!');
+    
+    // Inicializa componentes
+    initializeComponents();
+    
+    // Adiciona event listeners
+    addEventListeners();
+});
+
+/**
+ * Inicializa todos os componentes da aplicação
+ */
+function initializeComponents() {
+    console.log('Componentes inicializados');
+}
+
+/**
+ * Adiciona event listeners para interações do usuário
+ */
+function addEventListeners() {
+    console.log('Event listeners adicionados');
+}
